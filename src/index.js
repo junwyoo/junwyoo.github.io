42ff10e2e382e9ea902d0c951f450b63965c2b61
Junwhan Yoo <junwyoo@github.com>
2021-03-07 11:20:07 +0900

    add privacy agreement

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import html2canvas from 'html2canvas';
 import SignaturePad from 'signature_pad';
 
-var today = new Date().toLocaleDateString();
+var dateNow = new Date();
+var today = `${dateNow.getFullYear()}. ${dateNow.getMonth() + 1}. ${dateNow.getDate()}.`;
 
-new SignaturePad(document.getElementById('signature-pad'), {
+var registrationSign = new SignaturePad(document.getElementById('signature-pad'), {
     backgroundColor: 'rgba(255, 255, 255, 0)',
     penColor: 'rgb(0, 0, 0)'
 });
 
+var personalInfoSign = new SignaturePad(document.getElementById('signature-pad-personal-info'), {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    penColor: 'rgb(0, 0, 0)'
+});
+
+document.getElementById('clear-signature').onclick = function () {
+    registrationSign.clear();
+    personalInfoSign.clear();
+}
+
 document.body.onload = function () {
     document.getElementById('385405824').value = today;
+    document.getElementById('852124789').value = today;
     document.getElementById('2020695976').placeholder = today;
+}
+
+document.getElementById('119446736').onchange = function () {
+    document.getElementById('span-390780275').innerText = document.getElementById('119446736').value;
 }
 
 document.getElementById('save-to-pdf').onclick = function () {
